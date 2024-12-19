918727146219702b67d01713c4b73e69552f32f3
MJephat <mukundijephat22@gmail.com>
2024-12-19 10:57:16 +0300

    first step

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import "./Navbar.css";
+import MobileNav from '../mobileNav/mobileNav';
+
+const Navbar = () => {
+
+    const [ openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () =>{
+        setOpenMenu(!openMenu);
+    };
+
+  return (
+    <>
+    <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+
+    <nav className='nav-Wrapper'>
+        <div className="nav-content">
+            <img className='logo' src="./images/favicon.ico" alt="" />
+            <ul>
+                <li>
+                    <a className='menu-item'>Home</a>
+                    </li>
+                <li>
+                    <a className='menu-item'>Skills</a>
+                </li>
+                <li>
+                    <a className='menu-item'>Experience</a>
+                </li>
+                <li>
+                    <a className='menu-item'>Contact Me</a>
+                </li>
+
+                <button className='contact-btn' onClick={()=>{}}>
+                    Hire Me
+                </button>
+            </ul>
+
+            
+            <button class="menu-btn" onClick={toggleMenu}>
+            <span 
+                class="material-symbols-outlined"
+                style={{fontSize: "1.8rem"}}
+                >
+                    {openMenu ? "close": "menu"}
+                </span>
+            </button>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
